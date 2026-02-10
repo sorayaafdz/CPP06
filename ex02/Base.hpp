7834aa5ca17d4a52ddd66f00873465a888dca720
sorayaafdz <159011568+sorayaafdz@users.noreply.github.com>
2026-02-11 00:51:24 +0100

    Add files via upload

---
+++ ex02/Base.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Base.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: soraya <soraya@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/10 01:05:59 by soraya            #+#    #+#             */
+/*   Updated: 2026/02/10 01:38:27 by soraya           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BASE_HPP
+#define BASE_HPP
+
+class Base
+{
+	public:
+		virtual ~Base() {}
+};
+
+class A : public Base {};
+class B : public Base {};
+class C : public Base {};
+
+#endif
